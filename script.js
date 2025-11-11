849ee67ee2335ebfecc55ccf73bd8c2c510470e3
giseleesquarcini-glitch <gisele.esquarcini@escola.pr.gov.br>
2025-11-11 08:34:02 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ const avanca = document.querySelectorAll(".btn-proximo")
 
 avanca.forEach(button => {
     button.addEventListener('click', function () {
-        const atual = 'passo-' + this.getAttribute('data-proximo')
-
-        atual.classList.remove('ativo')
+        const atual = document.querySelector('.ativo');
+            const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+        
+            atual.classList.remove('ativo');
         document.getElementById(proximoPasso).classList.add('ativo')
     })
 })
